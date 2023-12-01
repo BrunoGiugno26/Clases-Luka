20425d890af496fe97ce96167acafcda71482223
BrunoGiugno26 <brunogiugno@gmail.com>
2023-12-01 13:20:37 -0300

    fetch

---
+++ Clase 9 - Fetch/Clase 27 de Noviembre.js
@@ -0,0 +1,29 @@
+//*FETCH: Javascript nos ofrece el metodo fetch para hacer peticiones https a algun servicio externo (API),como estas peticiones son asincronicas nosotros vamos a usar el metodo fetch para trabajar con promesas.
+
+// El metodo fetch recibe un primer parametro que es la url a la cual al hacer la peticion, y un segundo parametro opcional de configuracion que es un objeto
+
+//fetch("https://jsonplaceholder.typicode.com/users") //Promise {<Pendings>}
+//.then((response) => response.json()) // Promise {<pending>}
+//.then((data) => {
+//    console.log(data[2]["name"])
+//    console.log(data[2]["email"])
+//    console.log(data[2]["id"])
+//});
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then ((response) => response.json())
+.then ((data) => {
+    data.forEach((user) => {
+        const h2 = document.createElement("h2")
+        const h4 = document.createElement("h4")
+        const hr = document.createElement("hr")
+
+        h2.textContent = user.name
+        h4.textContent = user.email
+        hr.textContent = user.name.email
+
+        document.body.appendChild (h2)
+        document.body.appendChild (h4)
+        document.body.appendChild (hr)
+    })
+})
