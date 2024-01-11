c8916f1a651b2bc2e1cf4da92f7fa9cc93907b8e
BrunoGiugno26 <brunogiugno@gmail.com>
2024-01-11 11:29:58 -0300

    Clase del 27 de diciembre terminada

---
+++ teoria-practica/nodejs/clase1-http/index.js
@@ -0,0 +1,73 @@
+//const os = require("os")
+
+//console.log(os.homedir())
+
+//const fs = require("fs");
+
+//fs.readFile("hola.txt","utf8",(err,data) =>{
+//    if (err) throw err
+//    console.log(data)
+//})
+
+//fs.writeFile("prueba.txt","Hola mundo desde js!!", (err) => {
+//    if (err) throw err
+//    console.log("Archivo creado")
+//})
+
+//fs.unlink("prueba.txt", (err) => {
+//    if (err) {
+//        console.log(err)
+//    } else {
+//        console.log("Archivo eliminado")
+//    }
+//})
+
+//const http = require("http");
+
+//const server = http.createServer((req,res) => {
+//res.statusCode = 200
+//res.setHeader("Content-type","text/html")
+//res.end("<h1>hola mundo desde NodeJS</h1>")
+//})
+
+//server.listen(() => {
+//console.log("El servidor se esta ejecutando en http://localhost:8080")
+//})
+
+const http = require ("http")
+const url = require("url")
+
+const router = {
+    "/":(req,res) => {
+        console.log(req)
+        res.statusCode = 200
+        res.setHeader("Content-type","text/html")
+        res.end("<h1>Hola undo desde Nodejs</h1>")
+    },
+    "/about":(req,res) => {
+        res.statusCode = 200
+        res.setHeader("Content-type","text/html")
+        res.end("<h1>Acerca de Nosotros</h1>")
+    },
+    "/Contact":(req,res) => {
+        res.statusCode = 200
+        res.setHeader("Content-type","text/html")
+        res.end("<h1>Contactanos</h1>")
+    },
+}
+
+const server = http.createServer((req,res) => {
+   const path = url.parse(req.url).pathname
+
+    if (router[path]) {
+        router[path](req,res)
+    } else {
+        res.statusCode = 404
+       res.setHeader("Content-type","text/html")
+       res.end("<h1>404 Not Found</h1>")
+    }
+})
+
+server.listen(() => {
+console.log("El servidor esta escribiendo en el puerto 8080")
+})
